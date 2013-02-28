852e21f207bc6558f010fed8fae91b1f4c358398
User Vad83r <vad83r@bullit.z8.ru>
2013-03-01 00:15:50 +0400

    update 1.1.1.20533 delete backup file from versioning index

--- wa-data/protected/wa-installer/backup/wa-config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array (
-  'debug' => false,
-  'identity_hash' => 'e26b7476900338477522c934316dba61',
-  'mod_rewrite' => true,
-);
-//EOF
--- wa-data/protected/wa-installer/backup/wa-installer/lib/config/sources.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'apps' => 'http://www.webasyst.com/download/apps/list/',
-  'system' => 'http://www.webasyst.com/download/system/list/',
-);
